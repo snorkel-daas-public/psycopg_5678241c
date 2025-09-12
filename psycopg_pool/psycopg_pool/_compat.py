ab10bd720b74e85c1bb4ee4d63de23d71ccf8927
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2025-09-12 17:18:37 +0200

    chore: bump mypy, remove a now unneeded ignore

---
+++ psycopg_pool/psycopg_pool/_compat.py
@@ -0,0 +1,37 @@
+"""
+compatibility functions for different Python versions
+"""
+
+# Copyright (C) 2021 The Psycopg Team
+
+from __future__ import annotations
+
+import sys
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
+
+if sys.version_info >= (3, 13):
+    from typing import TypeVar
+else:
+    from typing_extensions import TypeVar
+
+import psycopg.errors as e
+
+__all__ = [
+    "Self",
+    "TypeVar",
+]
+
+# Workaround for psycopg < 3.0.8.
+# Timeout on NullPool connection mignt not work correctly.
+try:
+    ConnectionTimeout: type[e.OperationalError] = e.ConnectionTimeout
+except AttributeError:
+
+    class DummyConnectionTimeout(e.OperationalError):
+        pass
+
+    ConnectionTimeout = DummyConnectionTimeout
